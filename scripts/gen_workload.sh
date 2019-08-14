d1815809548dcda2bdabb43b99db077538a22201
SeKwonLee <sklee@cs.utexas.edu>
2019-08-13 22:07:23 -0700

    script updates

--- scripts/gen_workload.sh
+++ scripts/gen_workload.sh
@@ -2,8 +2,8 @@
 
 source scripts/compile_config.sh
 
-sudo apt-get install --assume yes build-essential cmake libboost-all-dev libpapi-dev
-sudo apt-get install --assume yes libtbb-dev libjemalloc-dev
+sudo apt-get -y install build-essential cmake libboost-all-dev libpapi-dev
+sudo apt-get -y install libtbb-dev libjemalloc-dev
 
 cd ./index-microbench
 curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.11.0/ycsb-0.11.0.tar.gz
