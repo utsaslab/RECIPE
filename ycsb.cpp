101d46891eb5c7752ddbbf03c2776d7c7e9b1485
Sekwon Lee <sekwonlee90@gmail.com>
2019-11-19 17:45:08 -0600

    change default workload size to be 64M

--- ycsb.cpp
+++ ycsb.cpp
@@ -223,8 +223,8 @@ void barrier_cross(barrier_t *b) {
 barrier_t barrier;
 /////////////////////////////////////////////////////////////////////////////////
 
-static uint64_t LOAD_SIZE = 10000000;
-static uint64_t RUN_SIZE = 10000000;
+static uint64_t LOAD_SIZE = 64000000;
+static uint64_t RUN_SIZE = 64000000;
 
 void loadKey(TID tid, Key &key) {
     return ;
